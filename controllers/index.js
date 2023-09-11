1a8d94a631af8d8f0a6eaa394d25cf6a7255d6de
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-09-11 21:05:04 +1000

    routes checked, removed unnecessary code that referenced other models

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const dashboardRoutes = require('./dashboard-routes.js');
-const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require("./api")
 
 router.use('/', homeRoutes);
