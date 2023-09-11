77d587c8d61cd4d6bc57849d24817d4ff631b7cb
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-09-11 20:57:19 +1000

    debugging heroku deployment jawsdb v2

--- config/connection.js
+++ config/connection.js
@@ -4,31 +4,21 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Connection to DB
-// let sequelize;
+let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql', // or 'postgres' for Postgres
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
