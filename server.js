2237ae8c0b550a5f652f2347a7764dbe147559bd
YJPuk <133621780+YJPuk@users.noreply.github.com>
2023-09-11 14:48:12 +1000

    adding structure to server.js, added schema for db, added helper for date and withAuth

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection.js");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: "?",
+    cookie: { maxAge: 2 * 24 * 60 * 60 * 1000 }, // 2 days in ms
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server on port ${PORT}!`));
+});
